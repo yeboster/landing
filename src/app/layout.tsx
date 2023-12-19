ddad6a0ce13c57fc741dc424d15ccafc38d9753b
Marco <marco@yeboster.com>
2023-12-19 15:04:25 +0100

    refactor: layout import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 import Link from 'next/link'
 import { Logo } from '@/components/ui/logo'
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
