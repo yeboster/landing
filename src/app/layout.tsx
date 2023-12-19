e33a092b0c9ec4649d5dd0bdfbfe0dcb5ba8bcf6
Marco <marco@yeboster.com>
2023-12-19 14:44:15 +0100

    refactor: navbar n footer into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
+import { Logo } from '@/components/ui/logo'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Yeboster',
-  description: 'My website',
+  description: 'My personal website',
 }
 
 export default function RootLayout({
@@ -16,7 +18,47 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div key="1" className="flex flex-col min-h-screen bg-[#d9e8f5]">
+          <header className="px-4 lg:px-6 h-16 flex items-center">
+            <Link className="flex items-center justify-center" href="#">
+              <Logo className="w-6 h-6" />
+              <span className="sr-only">Tech Portfolio</span>
+            </Link>
+            <nav className="ml-auto flex gap-4 sm:gap-6">
+              <Link className="text-sm font-medium hover:underline underline-offset-4" href="/">
+                Home
+              </Link>
+              <Link className="text-sm font-medium hover:underline underline-offset-4" href="/about">
+                About Me
+              </Link>
+              <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
+                Projects
+              </Link>
+              <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
+                Skills
+              </Link>
+              <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
+                Contact
+              </Link>
+            </nav>
+          </header>
+          {children}
+        </div>
+        <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+          <p className="text-xs text-gray-500 dark:text-gray-400">
+            © My Tech Portfolio. All rights reserved.
+          </p>
+          <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+            <Link className="text-xs hover:underline underline-offset-4" href="#">
+              Terms of Service
+            </Link>
+            <Link className="text-xs hover:underline underline-offset-4" href="#">
+              Privacy
+            </Link>
+          </nav>
+        </footer>
+      </body>
     </html>
   )
 }
