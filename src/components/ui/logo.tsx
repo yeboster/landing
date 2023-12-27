afc72924d1af1cfc611e114531d04f6f3efa455f
Marco <marco@yeboster.com>
2023-12-27 12:43:35 +0100

    refactor: improve logo props

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image"
+import Image, { ImageProps as NextImageProps } from "next/image"
 import logo from '../../../public/images/logo.png'
 import React from "react"
 
-export interface LogoProps {
-  width?: number | `${number}` | undefined;
-  height?: number | `${number}` | undefined;
-  fill?: boolean | undefined;
+export interface LogoProps
+  extends Omit<NextImageProps, 'src' | 'alt'> {
+  className?: string
 }
 
 const Logo = React.forwardRef<HTMLImageElement, LogoProps>((props, ref) => {
