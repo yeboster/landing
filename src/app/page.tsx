721169dfc030247e6eee3272f6c9e161cf8aae6f
Marco <marco@yeboster.com>
2023-12-27 16:18:11 +0100

    feat: add CTA on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+'use client'
+
+import { FadeInOnFirstScroll } from "@/components/ui/fade-in-on-first-scroll";
 import { Logo } from "@/components/ui/logo";
+import Link from "next/link";
 
 export default function Home(props: any) {
   return (
     <main className="flex-1">
       <section
-        className="w-full pt-12 md:pt-24 lg:pt-32 bg-center bg-cover"
+        className="w-full h-full py-12 md:py-24 lg:py-32 bg-center bg-cover"
       >
         <div className="flex flex-col items-center justify-center">
           <Logo
@@ -14,6 +18,82 @@ export default function Home(props: any) {
           <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">Yeboster</h1>
         </div>
       </section>
+      <FadeInOnFirstScroll
+        className="w-full py-12 md:py-24 lg:py-32 bg-center bg-cover bg-[#edf2f7]"
+      >
+        <div className="px-4 md:px-6 space-y-10 xl:space-y-16">
+          <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
+            <div>
+              <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
+                About Me
+              </h1>
+            </div>
+            <div className="flex flex-col items-start space-y-4">
+              <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+                A brief introduction about myself and my journey in the world.
+              </p>
+              <Link
+                className="inline-flex h-9 items-center justify-center rounded-md bg-[#1a202c] px-4 py-2 text-sm font-medium text-white shadow transition-colors hover:bg-[#2d3748] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#2d3748] disabled:pointer-events-none disabled:opacity-50 dark:bg-[#2d3748] dark:text-white dark:hover:bg-[#2d3748] dark:focus-visible:ring-[#2d3748]"
+                href="/about"
+              >
+                Learn more
+              </Link>
+            </div>
+          </div>
+        </div>
+      </FadeInOnFirstScroll>
+      <FadeInOnFirstScroll
+        className="w-full py-12 md:py-24 lg:py-32 bg-center bg-cover"
+      >
+        <div className="px-4 md:px-6 space-y-10 xl:space-y-16">
+          <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
+            <div>
+              <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
+                My Portfolio
+              </h1>
+            </div>
+            <div className="flex flex-col items-start space-y-4">
+              <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+                Discover my skills, projects, and my passion for technology.
+              </p>
+              <div className="space-x-4">
+                <Link
+                  className="inline-flex h-9 items-center justify-center rounded-md bg-[#1a202c] px-4 py-2 text-sm font-medium text-white shadow transition-colors hover:bg-[#2d3748] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#2d3748] disabled:pointer-events-none disabled:opacity-50 dark:bg-[#2d3748] dark:text-white dark:hover:bg-[#2d3748] dark:focus-visible:ring-[#2d3748]"
+                  href="/portfolio"
+                >
+                  Check it out
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </FadeInOnFirstScroll>
+      <FadeInOnFirstScroll
+        className="w-full py-12 md:py-24 lg:py-32 bg-center bg-cover bg-[#edf2f7]"
+      >
+        <div className="px-4 md:px-6 space-y-10 xl:space-y-16">
+          <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
+            <div>
+              <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
+                Get in Touch
+              </h1>
+            </div>
+            <div className="flex flex-col items-start space-y-4">
+              <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+                Feel free to reach out if you&apos;re looking for a developer, have a question, or just want to connect.
+              </p>
+              <div className="space-x-4">
+                <Link
+                  className="inline-flex h-9 items-center justify-center rounded-md bg-[#1a202c] px-4 py-2 text-sm font-medium text-white shadow transition-colors hover:bg-[#2d3748] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#2d3748] disabled:pointer-events-none disabled:opacity-50 dark:bg-[#2d3748] dark:text-white dark:hover:bg-[#2d3748] dark:focus-visible:ring-[#2d3748]"
+                  href="#" // TODO
+                >
+                  Contact Me
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </FadeInOnFirstScroll>
     </main>
   )
 }
