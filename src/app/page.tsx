22e713dc90e6c0d7260c95003fb1b6b157790f63
Marco <marco@yeboster.com>
2023-12-27 16:05:50 +0100

    feat: add logo into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
-export default function Home() {
+import { Logo } from "@/components/ui/logo";
+
+export default function Home(props: any) {
   return (
     <main className="flex-1">
       <section
         className="w-full pt-12 md:pt-24 lg:pt-32 bg-center bg-cover"
-        style={{
-          backgroundImage: "\"url('/path-to-theme-background-image.jpg')\"",
-        }}
-      ></section>
+      >
+        <div className="flex flex-col items-center justify-center">
+          <Logo
+            className="w-1/4"
+            sizes="40vw, 40vw"
+          />
+          <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">Yeboster</h1>
+        </div>
+      </section>
     </main>
   )
 }
