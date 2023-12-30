ec75cd3b5cd83d9106c682d0d5391a945da57978
Marco <marco@yeboster.com>
2023-12-30 10:59:59 +0100

    fix(mobile): homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,11 +25,11 @@ export default function Home(props: any) {
         <div className="px-4 md:px-6 space-y-10 xl:space-y-16">
           <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
             <div>
-              <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
+              <h1 className="lg:leading-tighter text-3xl font-bold text-center tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
                 About Me
               </h1>
             </div>
-            <div className="flex flex-col items-start space-y-4">
+            <div className="flex flex-col items-center md:items-start space-y-4">
               <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
                 A brief introduction about myself and my journey in the world.
               </p>
@@ -49,11 +49,11 @@ export default function Home(props: any) {
         <div className="px-4 md:px-6 space-y-10 xl:space-y-16">
           <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
             <div>
-              <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
+              <h1 className="lg:leading-tighter text-3xl font-bold text-center tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
                 My Portfolio
               </h1>
             </div>
-            <div className="flex flex-col items-start space-y-4">
+            <div className="flex flex-col items-center md:items-start space-y-4">
               <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
                 Discover my skills, projects, and my passion for technology.
               </p>
@@ -75,12 +75,12 @@ export default function Home(props: any) {
         <div className="px-4 md:px-6 space-y-10 xl:space-y-16">
           <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
             <div>
-              <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
+              <h1 className="lg:leading-tighter text-3xl font-bold text-center tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
                 Get in Touch
               </h1>
             </div>
-            <div className="flex flex-col items-start space-y-4">
-              <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+            <div className="flex flex-col items-center md:items-start space-y-4">
+              <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl text-center md:text-start dark:text-gray-400">
                 Feel free to reach out if you&apos;re looking for a developer, have a question, or just want to connect.
               </p>
               <div className="space-x-4">
